5b65d43a4cdcd1489094554907069d7c5f613965
Ashhad Rasool <ashhad.rasool22@gmail.com>
2023-12-12 10:53:40 +0500

    sunday reservation + corner cases fixes

--- script.py
+++ script.py
@@ -18,50 +18,6 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get(url)
 
 try:
-    select_parking_link = driver.find_element(By.XPATH, "//a[@href='/select-parking']")
-
-    select_parking_link.click()
-
-    calendar_selector = "//div[@role='grid'][@class='mbsc-calendar-table mbsc-calendar-table-active']"
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, calendar_selector)))
-    calendar_element = driver.find_element(By.XPATH, calendar_selector)
-    rows = calendar_element.find_elements(By.XPATH, ".//div[@role='row'][@class='mbsc-calendar-row']")
-
-    current_date = datetime.now()
-
-    days_until_saturday = (5 - current_date.weekday() + 7) % 7
-    date_on_saturday = current_date.date() + timedelta(days=days_until_saturday)
-
-    print(current_date.day)
-    print(current_date.date())
-    print('date_on_saturday', date_on_saturday)
-
-    if current_date.month == date_on_saturday.month:
-        days = calendar_element.find_elements(By.XPATH, ".//div[@class='mbsc-calendar-cell-text mbsc-calendar-day-text mbsc-ios']")
-        for day in days:
-            print(day.text)
-            if day.text == str(date_on_saturday.day):
-                day.click()
-                break
-
-        parking_option_element_selector = '//div[@class="RatesPanel_ratesWrapper__1peNP"]'
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, parking_option_element_selector)))
-        parking_option_div_element = driver.find_element(By.XPATH, parking_option_element_selector)
-
-        parking_options_selector = '//div[@class="RatesPanel_rateCopy__YOyL0"]'
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, parking_options_selector)))
-        parking_options = parking_option_div_element.find_elements(By.XPATH, parking_options_selector)
-
-
-        for option in parking_options:
-            if option.text == 'Advanced Paid Reservations':
-                driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-                time.sleep(2)
-                option.click()
-                break
-
-
-    time.sleep(2)
     login_button_selector = '//a[@href="/login"]'
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, login_button_selector)))
     login_button = driver.find_element(By.XPATH, login_button_selector)
@@ -76,7 +32,119 @@ try:
     login_button = driver.find_element(By.XPATH, '//button[text()="Login"]')
     login_button.click()
 
-    print()
+    select_parking_selector = "//a[@href='/parking-codes']"
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, select_parking_selector)))
+    select_parking_link = driver.find_element(By.XPATH, select_parking_selector)
+
+    select_parking_link.click()
+
+    current_date = datetime.now()
+
+    days_until_saturday = (5 - current_date.weekday() + 7) % 7
+    date_on_saturday = current_date.date() + timedelta(days=days_until_saturday)
+
+    days_until_sunday = days_until_saturday + 1
+    date_on_sunday = current_date.date() + timedelta(days=days_until_sunday)
+
+    # print('date_on_saturday', date_on_saturday)
+
+    calendar_selector = "//div[@role='grid'][@class='mbsc-calendar-table mbsc-calendar-table-active']"
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, calendar_selector)))
+
+    if current_date.month != date_on_saturday.month:
+        nextMonthButton = driver.find_element(By.XPATH, "//button[@class='mbsc-calendar-button mbsc-calendar-button-next mbsc-reset mbsc-font mbsc-button mbsc-ios mbsc-ltr mbsc-button-flat mbsc-icon-button']")
+        nextMonthButton.click()
+
+        time.sleep(5)
+
+
+    calendar_element = driver.find_element(By.XPATH, calendar_selector)
+    rows = calendar_element.find_elements(By.XPATH, ".//div[@role='row'][@class='mbsc-calendar-row']")
+    days = calendar_element.find_elements(By.XPATH, ".//div[@class='mbsc-calendar-cell-text mbsc-calendar-day-text mbsc-ios']")
+
+    for day in days:
+        if day.text == str(date_on_saturday.day):
+            day.click()
+            break
+
+    free_parking_on_saturday = False
+    free_parking_on_sunday = False
+
+    reservations_present = False
+    try:
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@class="RatesPanel_emptyRatesHeadline__QNAAY"]')))
+    except:
+        reservations_present = True
+
+    if reservations_present == True:
+        parking_option_element_selector = '//div[@class="RatesPanel_ratesWrapper__1peNP"]'
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, parking_option_element_selector)))
+        parking_option_div_element = driver.find_element(By.XPATH, parking_option_element_selector)
+
+        parking_options_selector = '//div[@class="RatesPanel_rateCopy__YOyL0"]'
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, parking_options_selector)))
+        parking_options = parking_option_div_element.find_elements(By.XPATH, parking_options_selector)
+
+        for option in parking_options:
+            if option.text == 'Free Reservations':
+                free_parking_on_saturday = True
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+                time.sleep(2)
+                option.click()
+                break
+
+    if(free_parking_on_saturday == True):
+        print("Saturday - " + date_on_saturday + ":Free Parking Reserved ")
+    else:
+        print("Saturday - " + date_on_saturday + ": No Free Parking Available")
+
+    driver.get('https://reservenski.parkpalisadestahoe-village.com/select-parking')
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, calendar_selector)))
+
+    if current_date.month != 1:
+        nextMonthButton = driver.find_element(By.XPATH, "//button[@class='mbsc-calendar-button mbsc-calendar-button-next mbsc-reset mbsc-font mbsc-button mbsc-ios mbsc-ltr mbsc-button-flat mbsc-icon-button']")
+        nextMonthButton.click()
+
+        time.sleep(5)
+
+    calendar_element = driver.find_element(By.XPATH, calendar_selector)
+    rows = calendar_element.find_elements(By.XPATH, ".//div[@role='row'][@class='mbsc-calendar-row']")
+    days = calendar_element.find_elements(By.XPATH, ".//div[@class='mbsc-calendar-cell-text mbsc-calendar-day-text mbsc-ios']")
+
+    for day in days:
+        if day.text == str(date_on_sunday.day):
+            day.click()
+            break
+
+    reservations_present = False
+
+    try:
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@class="RatesPanel_emptyRatesHeadline__QNAAY"]')))
+    except:
+        reservations_present = True
+
+    if reservations_present == True:
+        parking_option_element_selector = '//div[@class="RatesPanel_ratesWrapper__1peNP"]'
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, parking_option_element_selector)))
+        parking_option_div_element = driver.find_element(By.XPATH, parking_option_element_selector)
+
+        parking_options_selector = '//div[@class="RatesPanel_rateCopy__YOyL0"]'
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, parking_options_selector)))
+        parking_options = parking_option_div_element.find_elements(By.XPATH, parking_options_selector)
+
+        for option in parking_options:
+            if option.text == 'Free Reservations':
+                free_parking_on_sunday = True
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+                time.sleep(2)
+                option.click()
+                break
+
+    if(free_parking_on_sunday == True):
+        print("Sunday - " + date_on_sunday + ": Free Parking Reserved")
+    else:
+        print("Sunday - " + date_on_sunday + ": No Free Parking Available")
+
 
 finally:
     driver.quit()
